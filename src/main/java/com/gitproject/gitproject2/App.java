1eaa2cc09f8268f3e553353c241486b08c07b59a
chithaluri chandrashekar <chandrashekar976@gmail.com>
2023-05-31 20:36:26 +0530

    first commit

---
+++ src/main/java/com/gitproject/gitproject2/App.java
@@ -0,0 +1,16 @@
+package com.gitproject.gitproject2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    public void added(String main) {
+    	System.out.println("mvn project ");
+    }
+}
